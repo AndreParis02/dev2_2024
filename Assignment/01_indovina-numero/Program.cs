fdd4121a98353d97501c5d1622ba5ec75f3a365c
AndreParis02 <paris.andrea14@gmail.com>
2024-10-24 14:35:12 +0200

    Indovina numero: Versione 5

--- Assignment/01_indovina-numero/Program.cs
+++ Assignment/01_indovina-numero/Program.cs
@@ -1,39 +1,191 @@
 ﻿Random random = new Random();// Random e la classe che genera numeri casuali
-int numeroDaIndovinare = random.Next(1, 101);// Next e il metodo che genera un numero casuale tra 1 e 100
+int numeroInserito;
+int totPunti;
+int numPunti;
+int numeroDaIndovinare;
 
 Console.Clear();
 
-Console.WriteLine("Indovina il numero (tra 1 e 100): ");
+Console.WriteLine("scegli il livello di difficolta':");
+Console.WriteLine("difficoltà facile (f) Indovina il numero (tra 1 e 50),20 tentativi, 100 punti totali");
+Console.WriteLine("difficoltà media (m) Indovina il numero (tra 1 e 100),10 tentativi, 100 punti totali");
+Console.WriteLine("difficoltà difficile (d) Indovina il numero (tra 1 e 500),5 tentativi, 500 punti totali");
+Console.WriteLine("difficoltà impossibile (i) Indovina il numero (tra 1 e 1000),5 tentativi, 1000 punti totali");
 
-int numeroInserito;
-int totPunti = 100;
-int numPunti = totPunti;
-
-for(int i = 0; i < (totPunti/5); i++)
+string difficoltà = Console.ReadLine();
+switch (difficoltà)
 {
-    numeroInserito = Convert.ToInt32(Console.ReadLine());
-    numPunti -= 5;
+    case "f":
+        Console.WriteLine("Hai scelto la modalità facile");
 
-        if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+        numeroDaIndovinare = random.Next(1, 51);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 50): ");
+
+        totPunti = 100;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/5); i++)
         {
-            if(numeroInserito < numeroDaIndovinare)
-            {
-                Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
-                Console.WriteLine("Riprova: ");
-            }else
-            {
-                Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
-                Console.WriteLine("Riprova: ");
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 5;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/5;
+                }   
+                
             }    
-        }
-        else if(numeroInserito == numeroDaIndovinare)
+        if( numPunti == 0)
         {
-            Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
-            i = totPunti/5;
-        }   
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+        break;
+
+    case "m":
         
-    }    
-if( numPunti == 0)
-{
-    Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+         Console.WriteLine("Hai scelto la modalità media");
+
+        numeroDaIndovinare = random.Next(1, 101);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 100): ");
+
+        totPunti = 100;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/10); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 10;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/10;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+
+        break;
+
+         case "d":
+        
+         Console.WriteLine("Hai scelto la modalità difficile");
+
+        numeroDaIndovinare = random.Next(1, 501);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 500): ");
+
+        totPunti = 500;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/100); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 100;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/100;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+
+        break;
+
+         case "i":
+        
+         Console.WriteLine("Hai scelto la modalità impossibile");
+
+        numeroDaIndovinare = random.Next(1, 1001);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 1000): ");
+
+        totPunti = 1000;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/200); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 200;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/200;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+
+        break;
+
+    default:
+        Console.WriteLine("non hai selezionato nessun livello di difficoltà!");
+        break;
 }
--- Assignment/01_indovina-numero/README.md
+++ Assignment/01_indovina-numero/README.md
@@ -220,14 +220,202 @@ Permetti all'utente di scegliere tra i diversi livelli di difficoltà che modifi
 
 **Istruzioni:**
 
-* inizia con un punteggio massimo (es. 100 punti)
-* Ad ogni tentativo fallito, sottrai un certo numero di punti (es. 20 punti)
-* Alla fine del gioco mostra il punteggio dell'utente
+* in base ai livelli di difficoltà sottrai un tot di punti
 
 >**Esempio codice:**
 
 ```csharp
+Random random = new Random();// Random e la classe che genera numeri casuali
+int numeroInserito;
+int totPunti;
+int numPunti;
+int numeroDaIndovinare;
 
+Console.Clear();
+
+Console.WriteLine("scegli il livello di difficolta':");
+Console.WriteLine("difficoltà facile (f) Indovina il numero (tra 1 e 50),20 tentativi, 100 punti totali");
+Console.WriteLine("difficoltà media (m) Indovina il numero (tra 1 e 100),10 tentativi, 100 punti totali");
+Console.WriteLine("difficoltà difficile (d) Indovina il numero (tra 1 e 500),5 tentativi, 500 punti totali");
+Console.WriteLine("difficoltà impossibile (i) Indovina il numero (tra 1 e 1000),5 tentativi, 1000 punti totali");
+
+string difficoltà = Console.ReadLine();
+switch (difficoltà)
+{
+    case "f":
+        Console.WriteLine("Hai scelto la modalità facile");
+
+        numeroDaIndovinare = random.Next(1, 51);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 50): ");
+
+        totPunti = 100;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/5); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 5;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/5;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+        break;
+
+    case "m":
+        
+         Console.WriteLine("Hai scelto la modalità media");
+
+        numeroDaIndovinare = random.Next(1, 101);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 100): ");
+
+        totPunti = 100;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/10); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 10;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/10;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+
+        break;
+
+         case "d":
+        
+         Console.WriteLine("Hai scelto la modalità difficile");
+
+        numeroDaIndovinare = random.Next(1, 501);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 500): ");
+
+        totPunti = 500;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/100); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 100;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/100;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+
+        break;
+
+         case "i":
+        
+         Console.WriteLine("Hai scelto la modalità impossibile");
+
+        numeroDaIndovinare = random.Next(1, 1001);
+
+        Console.WriteLine("Indovina il numero (tra 1 e 1000): ");
+
+        totPunti = 1000;
+        numPunti = totPunti;
+
+
+        for(int i = 0; i < (totPunti/200); i++)
+        {
+            numeroInserito = Convert.ToInt32(Console.ReadLine());
+            numPunti -= 200;
+
+                if(numeroInserito != numeroDaIndovinare && numPunti != 0)
+                {
+                    if(numeroInserito < numeroDaIndovinare)
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' maggiore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }else
+                    {
+                        Console.WriteLine($"Il numero da indovinare e' minore, hai ancora {numPunti} punti!");
+                        Console.WriteLine("Riprova: ");
+                    }    
+                }
+                else if(numeroInserito == numeroDaIndovinare)
+                {
+                    Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}, indovinato con {numPunti} punti!");
+                    i = totPunti/200;
+                }   
+                
+            }    
+        if( numPunti == 0)
+        {
+            Console.WriteLine($"Peccato non hai indovinato ed hai finito tutti i punti! Il numero da indovinare era: {numeroDaIndovinare}");
+        }
+
+        break;
+
+    default:
+        Console.WriteLine("non hai selezionato nessun livello di difficoltà!");
+        break;
+}
 ```
 ### Comandi versionamento
 
