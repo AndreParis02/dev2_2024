bacb47ee46576bc5a6060a0309da01d397e5f5d1
AndreParis02 <paris.andrea14@gmail.com>
2024-12-02 16:22:06 +0100

    18_pers-csv

--- xx-Esercitazioni/15_metodi-files/Program.cs
+++ xx-Esercitazioni/15_metodi-files/Program.cs
@@ -38,3 +38,107 @@ Console.WriteLine(tempFile);
 //Path.Combine unisce i path in questo caso aggiunge "temp" alla directory temporanea
 string tempDir= Path.Combine(Path.GetTempPath(),"temp");
 Directory.CreateDirectory(tempDir);
+
+// verificare se un file esiste (restituisce un valore booleano)
+if (File.Exists(path))
+{
+    Console.WriteLine("File exists");
+}
+
+// verificare se una directory esiste
+if (Directory.Exists(dir))
+{
+    Console.WriteLine("Directory exists");
+}
+
+// ottenere informazioni su un file
+FileInfo info = new FileInfo(path);
+Console.WriteLine(info.Length);
+Console.WriteLine(info.CreationTime);
+
+// fare riferimento solo al nome del file senza il path
+string fileName = Path.GetFileName(path);
+Console.WriteLine(fileName);
+
+// fare riferimento solo all'estensione del file
+string extension = Path.GetExtension(path);
+Console.WriteLine(extension);
+
+// fare riferimento solo al nome del file senza l'estensione
+string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(path);
+Console.WriteLine(fileNameWithoutExtension);
+
+// ottenere informazioni su una directory
+DirectoryInfo dirInfo = new DirectoryInfo(dir);
+Console.WriteLine(dirInfo.CreationTime);
+
+// ottenere informazioni su tutti i file in una directory
+string[] files = Directory.GetFiles(dir); // dir e il path della directory
+                                          // se voglio partire dalla cartella del progetto di dotnet nel quale sono scrivo string dir = ".";
+foreach (string file in files)
+{
+    Console.WriteLine(file);
+}
+
+// ottenere informazioni su tutte le directory in una directory
+string[] dirs = Directory.GetDirectories(dir);
+foreach (string d in dirs)
+{
+    Console.WriteLine(d);
+}
+
+// ottenere informazioni su tutti i file e le directory in una directory
+string[] all = Directory.GetFileSystemEntries(dir);
+foreach (string a in all)
+{
+    Console.WriteLine(a);
+}
+
+// ottenere informazioni su tutti i file e le directory in una directory con un filtro
+string[] txtFiles = Directory.GetFiles(dir, "*.txt");
+foreach (string txtFile in txtFiles)
+{
+    Console.WriteLine(txtFile);
+}
+
+// creare una copia di un file
+string copyPath = Path.Combine(dir, "test.txt"); // Path.Combine unisce i path
+File.Copy(path, copyPath);
+
+// spostare un file
+string movePath = Path.Combine(dir, "test2.txt");
+File.Move(copyPath, movePath);
+
+// eliminare un file
+File.Delete(movePath);
+
+// eliminare una directory e tutti i file e le directory al suo interno
+Directory.Delete(dir, true);
+
+// eliminare tutti i file in una directory
+string[] files1 = Directory.GetFiles(dir);
+foreach (string file in files1)
+{
+    File.Delete(file);
+}
+
+// eliminare tutti i file e le directory in una directory
+string[] all1 = Directory.GetFileSystemEntries(dir);
+foreach (string a in all1)
+{
+    if (File.Exists(a))
+    {
+        File.Delete(a);
+    }
+    else
+    {
+        Directory.Delete(a, true);
+    }
+}
+
+// eliminare tutti i file e le directory in una directory con un filtro
+string[] txtFiles1 = Directory.GetFiles(dir, "*.txt");
+foreach (string txtFile in txtFiles1)
+{
+    File.Delete(txtFile);
+}
---
+++ xx-Esercitazioni/18_pers-csv/Program.cs
@@ -0,0 +1,96 @@
+﻿//GESTIONE FILES CSV
+
+// ESEMPIO DI FILE CSV
+
+//prodotto,quantità,prezzo
+//Macchina,11,30
+//Mouse,10,25
+
+//LEGGERE UN CONTENUTO DA UN FILE CSV
+
+string path =@"test.csv"; //in questo caso il file è nella stessa cartella del programma
+string [] lines = File.ReadAllLines(path); // legge tutte le righe del file e le mette in un array di stringhe
+
+foreach(string line in lines)
+{
+    Console.WriteLine(line); // stampa la riga
+}
+
+// creare una lista di stringhe partendo dal file csv
+
+List<string> list =new List<string>();
+
+foreach (string line in lines)
+{
+    list.Add(line);
+}
+
+//creare una file CSV con il nome del file che corrisponde al nome della prima colonna
+//ed il contenuto del file che corrisponde al contenuto delle altre colonne disponibili
+
+string[][] data = new string[lines.Length][];
+for(int i = 1;i < lines.Length;i++)
+{
+    data[i] = lines[i].Split(','); //divide la riga in base alla virgola e mette i pezzi in un array di stringhe
+}
+
+for (int i = 1; i < data.Length; i++)
+{
+    string path2 = data[i][0] + ".csv"; // il nome del file è il nome del prodotto
+    File.Create(path2).Close(); // crea il file e lo chiude subito
+    
+    for(int j = 1;j < data[i].Length;j++)
+    {
+        File.AppendAllText(path2, data[i][j] + "\n");
+    }
+}
+
+//inserire i dati dentro il file tramite input utente
+
+Console.WriteLine("inserisci i dati da mettere all'interno del file (separandoli con una ',') prodotto, quantità, prezzo");
+string inserimento = Console.ReadLine();
+File.AppendAllText(path,inserimento + "\n");
+
+//eliminare un elemento specifico da un file csv (prodotto, quantità, prezzo)
+
+Console.WriteLine("inserisci i dati da eliminare (separandoli con una ',') prodotto, quantità, prezzo");
+string eliminare = Console.ReadLine();
+string[] lines2 = File.ReadAllLines(path);
+File.Create(path).Close(); //bisogna creare il file perchè altrimenti non si cancella il contenuto
+foreach(string line in lines2)
+{
+    string[] data2 = line.Split(',');
+    if(data2[0] != eliminare)
+    {
+        File.AppendAllText(path, line + "\n");
+    }
+}
+
+//modificare un elemento specifico da un file csv (prodotto, quantità, prezzo)
+
+Console.WriteLine("inserisci i dati da modificare (separandoli con una ',') prodotto, quantità, prezzo");
+string modificare = Console.ReadLine();
+string[] lines3 = File.ReadAllLines(path);
+string[] temp = new string[lines3.Length];
+File.Create(path).Close(); //bisogna creare il file perchè altrimenti non si cancella il contenuto
+
+for(int i = 0;i < lines3.Length; i++)
+{
+    if(modificare != lines3[i])
+    {
+        temp[i] = lines3[i];
+    }
+    else
+    {
+        Console.WriteLine("inserisci i dati modificati (separandoli con una ',') prodotto, quantità, prezzo"); 
+        string modifica = Console.ReadLine();  
+        temp[i] = modifica;
+    }
+    File.AppendAllText(path,temp[i] + "\n");
+}
+
+
+
+
+
+  
