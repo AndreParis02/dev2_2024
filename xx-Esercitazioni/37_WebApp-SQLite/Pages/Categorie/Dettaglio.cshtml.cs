3576aef2d35ed00932f534a8e33f168f13eb51dc
AndreParis02 <paris.andrea14@gmail.com>
2025-02-13 15:58:34 +0100

    Creazione Operazioni CRUD Per Categorie

---
+++ xx-Esercitazioni/37_WebApp-SQLite/Pages/Categorie/Dettaglio.cshtml.cs
@@ -0,0 +1,44 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data.SQLite;
+
+namespace _37_WebApp_SQLite.Pages.Categorie;
+
+public class DettaglioModel : PageModel
+{
+    public Categoria Categoria { get; set; }
+
+    public IActionResult OnGet(int id)
+    {
+        using var connection = DatabaseInitializer.GetConnection();
+        connection.Open();
+
+        
+       
+        var sql = @"SELECT Id, Nome FROM  Categorie WHERE Id = @id";
+
+        using var command = new SQLiteCommand(sql, connection);
+        command.Parameters.AddWithValue("@id", id);
+
+        using var reader = command.ExecuteReader();
+
+
+        if (reader.Read())
+        {
+            Categoria = new Categoria
+            {
+                Id = reader.GetInt32(0),
+                Nome = reader.GetString(1),
+            };
+        }
+        else
+        {
+            return NotFound(); // Se non trova il prodotto, ritorna NotFound
+        }
+
+        return Page();
+    }
+    
+}
