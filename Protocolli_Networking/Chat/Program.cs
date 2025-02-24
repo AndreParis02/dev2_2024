782acb80d5f1d1f7df846f5b840181f32b2a354c
AndreParis02 <paris.andrea14@gmail.com>
2025-02-24 11:18:20 +0100

    fine chat Client-Server

--- Protocolli_Networking/Chat/Program.cs
+++ Protocolli_Networking/Chat/Program.cs
@@ -3,31 +3,91 @@ using System.Text; //using che da le funzionalità per l gestione delle stringhe
 
 public class Client
 {
+    private TcpClient client; //Oggetto che rappresenta il client TCP
+    private NetworkStream stream;//Oggetto che rappresenta il flusso di dati tra il client e il server
+
     //metodo per stabilire la connessione con il server
-    void StartClient(string serverIP, int port) //gli argomenti sono l''indirizzo IP del server e la porta su cui il server è in ascolto
+    public void StartClient(string serverIP, int port) //gli argomenti sono l''indirizzo IP del server e la porta su cui il server è in ascolto
     {
-        using (var client = new TcpClient(serverIP, port)) //TcpClient è la classe che rappresenta un client TCP cioè un client che si connette a un server tramite il protocollo TCP
-
-        using  (var stream = client.GetStream()) //GetStream restituisce un oggetto NetworkStream che rappresenta il flusso di dati tra il client e il server
+        try
         {
+
+
+            client = new TcpClient(serverIP, port); //TcpClient è la classe che rappresenta un client TCP cioè un client che si connette a un server tramite il protocollo TCP
+
+            stream = client.GetStream(); //GetStream restituisce un oggetto NetworkStream che rappresenta il flusso di dati tra il client e il server
             Console.WriteLine("Connesso al server.");
-            string messageToSend = Console.ReadLine();
 
-            while (!string.IsNullOrEmpty(messageToSend))
-            {
-                byte[] buffer = Encoding.ASCII.GetBytes(messageToSend); //converte la stringa in un array di byte
-                stream.Write(buffer, 0, buffer.Length);
+            Thread receiveThread = new Thread(ReciveMessages);
+            receiveThread.Start();
 
-                messageToSend = Console.ReadLine(); //Legge il messaggio da inviare
-            }
+            SendMessages();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Errore di connessione: {ex.Message}");
         }
     }
 
-    public static void Main(string [] args)
+    private void SendMessages()
+    {
+        try
+        {
+            string messageToSend;
+
+            while (!string.IsNullOrEmpty(messageToSend = Console.ReadLine()))
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(messageToSend);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Errore nell'invio del messaggio: {ex.Message}");
+        }
+        finally
+        {
+            CloseConnection();
+        }
+    }
+
+    private void ReciveMessages()
+    {
+        byte[] buffer = new byte[1024];
+
+        try
+        {
+            int byteCount;
+            while((byteCount = stream.Read(buffer, 0, buffer.Length)) !=0)
+            {
+                string message = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                Console.WriteLine($"Messaggio ricevuto dal server: {message}");
+            }
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("Connessione al server interrotta.");
+        }
+        finally
+        {
+            CloseConnection();
+        }
+    }
+
+    private void CloseConnection()
+    {
+        if(client != null)
+        {
+            client.Close();
+            Console.WriteLine("Connessione chiusa.");
+        }
+    }
+
+    public static void Main(string[] args)
     {
         Client client = new Client(); //Crea un'istanza della classe Client in modo da poter chiamare il metodo StartClient
         Console.WriteLine("inserisci l'IP del server:"); //Chiede al'utente di inserire l'IP del server
         string serverIP = Console.ReadLine(); //Legge l'IP del server inserito dall'utente
-        client.StartClient(serverIP,3000); //Avvia il client con l'IP del server e la porta 3000
+        client.StartClient(serverIP, 3000); //Avvia il client con l'IP del server e la porta 3000
     }
 }
--- Protocolli_Networking/Chat_Server/Program.cs
+++ Protocolli_Networking/Chat_Server/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System.Net;
 using System.Net.Sockets; //using che da le funzionalità per la comunicazione di rete
 using System.Text; //using che da le funzionalità per l gestione delle stringhe che vengono inviate e ricevute
-using System.Threading;
-using System.Threading.Tasks.Dataflow; //unsing che da le funzionalità per la gestione dei thread cioè dei flussi di esecuzione separati
+using System.Threading; //unsing che da le funzionalità per la gestione dei thread cioè dei flussi di esecuzione separati
 
 public class Server
 {
     private TcpListener listener; //Oggetto che rappresenta un server TCP
+
+    //creo la lista clients per memorizzare i client
+    private List<TcpClient> clients = new List<TcpClient>(); //Lista per memorizzare i client connessi
+
+    private object lockObject = new object(); //proprietà Lock per evitare problemi di concorrenza
     public void StartServer(int port)
     {
         listener = new TcpListener(IPAddress.Any, port); // IPAddress.Any indica che il server accetta connessioni su tutte le interfacce di rete
         listener.Start(); //Avvia il server
+        Console.WriteLine($"Server avviato su porta {port}");
 
         while (true)
         {
             TcpClient client = listener.AcceptTcpClient(); //Accetta una connessione da un client e restituisce un oggetto TcpClient che rappresenta il client connesso
+            lock (lockObject)
+            {
+                clients.Add(client);//Aggiunge un client alla lista
+            }
             Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient)); //Crea un nuovo thread per gestire il client connesso
             clientThread.Start(client);
         }
@@ -26,19 +35,31 @@ public class Server
         NetworkStream stream = client.GetStream(); //Ottiene il flusso di dati tra il client e il server networkstream
         byte[] buffer = new byte[1024];
 
-        int byteCount;
-
-        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+        try
         {
-            string message = Encoding.ASCII.GetString(buffer, 0, byteCount); //converte i byte ricevuti in una stringa
-            Console.WriteLine($"Messaggio ricevuto: {message}");
-            Broadcast(message); //Invia il messaggio a tutti i client connessi
-        }
-        client.Close();
-    }
+            int byteCount;
 
-    //creo la lista clients per memorizzare i client
-    private List<TcpClient> clients = new List<TcpClient>(); //Lista per memorizzare i client connessi
+            while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string message = Encoding.ASCII.GetString(buffer, 0, byteCount); //converte i byte ricevuti in una stringa
+                Console.WriteLine($"Messaggio ricevuto: {message}");
+                Broadcast(message); //Invia il messaggio a tutti i client connessi
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Un client si è disconnesso.");
+        }
+        finally
+        {
+            lock (lockObject)
+            {
+                clients.Remove(client);
+            }
+            client.Close();
+        }
+
+    }
 
     //il metodo Broadcast si occupa di inviare un messaggio a tutti i client connessi
     private void Broadcast(string message)
@@ -51,7 +72,7 @@ public class Server
         }
     }
 
-    public static void Main (string[]args) //Metodo Main che viene eseguito all'avvio del programma
+    public static void Main(string[] args) //Metodo Main che viene eseguito all'avvio del programma
     {
         Server server = new Server(); //Crea un'istanza della classe Server
         server.StartServer(3000); // Avvia il server sulla porta 3000
